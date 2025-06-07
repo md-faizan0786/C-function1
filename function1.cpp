e33dfdc25ca46591b0022a8413411ec34e2e9728
Md Faizan <mdfaizankurisaray@gmail.com>
2025-06-07 22:34:20 +0530

    first commit

---
+++ function1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void sayHello() {
+    cout<< "Hello :)\n";
+}
+
+void assistant() {
+    sayHello();
+    cout << "work done \n";
+}
+
+int main() {
+
+    assistant();
+
+    return 0;
+}
