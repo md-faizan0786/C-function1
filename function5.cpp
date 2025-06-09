93b6d8364fc54a94200da6cc86952046f79b2450
Md Faizan <mdfaizankurisaray@gmail.com>
2025-06-09 22:18:21 +0530

    5th commit

---
+++ function5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+bool isPrime2(int n) {
+    if(n==1) {
+        return false;
+    }
+
+    for(int i=2; i*i<=n; i++) {
+        if(n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+
+    cout << isPrime2(10) << endl;
+
+    return 0;
+}
